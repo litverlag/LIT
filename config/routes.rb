72bac1ce2f273e5622abdba5adc85e2ef42ea65d
Rouven Glauert <homepage@lit-verlag.de>
2015-10-23 09:22:08 +0200

    Message

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'admin/projekts/new', :to => 'projekts#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   # root 'admin#login'
   root to: "admin/dashboard#index"
