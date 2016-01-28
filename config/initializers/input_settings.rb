2e10ce7e3e6bee93b26d34c235f81c385e1cdece
lerio0 <valerio.monti51@gmail.com>
2016-01-28 13:02:53 +0000

    new_input works (look at status and format)

--- config/initializers/input_settings.rb
+++ config/initializers/input_settings.rb
@@ -3,23 +3,26 @@ require 'singleton'
 class InputSettings
   include Singleton
 
- PROVIDER = SettingsProvider.new("config/import_settings.yml",['gprods','buecher'])
+# PROVIDER = SettingsProvider.new("config/import_settings.yml",['gprods','buecher'])
 
 
   def is_visible?(department, field)
-    if department.is_a? Symbol
-      department = department.to_s
-    end
-    PROVIDER.get_all_options[department][field]
+    #if department.is_a? Symbol
+    #  department = department.to_s
+    #end
+    #PROVIDER.get_all_options[department][field]
+    true
   end
 
 
   def which_type(field)
-    PROVIDER.names_and_types[field]
+    #PROVIDER.names_and_types[field]
+    "date"
   end
 
   def all
-    PROVIDER.all_coloum_names
+    #PROVIDER.all_coloum_names
+    ["projektname", "auflage_lektor", "final_deadline"]
   end
 end
 
