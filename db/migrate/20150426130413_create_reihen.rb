e630fd839d5099a5f0dfe47685b649a329a65b03
PC beim LIT Verlag <homepage@lit-verlag.de>
2016-07-22 15:05:58 +0200

    Do not pay attention to this commit.
    Srsly.

--- db/migrate/20150426130413_create_reihen.rb
+++ db/migrate/20150426130413_create_reihen.rb
@@ -3,6 +3,7 @@ class CreateReihen < ActiveRecord::Migration
     create_table :reihen do |t|
       t.string :name
       t.string :r_code
+      t.string :issn	#Should only be in 'reihen' db.
 
       t.timestamps null: false
     end
