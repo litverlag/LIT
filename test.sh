54eb6fc7d9a96b2d3fd5a8658aaa2a423b0d3fae
lit-development <voss@lit-verlag.de>
2016-09-21 12:20:57 +0200

    unless nil and not nil

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 [ -x bin/rake ] || exit 1
 bin/rake gapi:test
+exit_1=$?
 bin/rake test
+exit_2=$?
 if [[ "x$1" == "xcommit" ]] ; then
+	if [ $exit_1 -eq 1 ] || [ $exit_2 -eq 1 ]; then
+		echo errors happended during testing i wont commit that..
+		exit 1
+	fi
 	git commit -a
 	#git push --all
 fi
