784927ba01cb25688a011420cdff080638da23b1
lit-development <voss@lit-verlag.de>
2016-11-28 18:32:39 +0100

    To be continued, have to leave now.

--- app/models/externer_druck.rb
+++ app/models/externer_druck.rb
@@ -2,8 +2,8 @@
 class ExternerDruck < Gprod
 	# Crappy SQL join..
   def self.default_scope
-    ExternerDruck.joins("INNER JOIN status_titelei on status_titelei.gprod_id = gprods.id")\
-			.where("status_titelei.status IS NOT NULL")
+    ExternerDruck.joins("INNER JOIN status_druck on status_druck.gprod_id = gprods.id")\
+    .where("status_druck.status IS NOT NULL")
   end
   scope_maker([:neu_filter, :bearbeitung_filter, :verschickt_filter, :fertig_filter, :problem_filter], "status_externer_druck", StatusOptionsAdapter.option(:statusexternerdruck))
 end
